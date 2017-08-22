a33ff6cf317ba9c4d733a4f3e094c3d239ea15fe
unknown <mrashes2@gmail.com>
2017-08-21 20:02:54 -0500

    sudocoding roomcode comparison

--- public/js/player.js
+++ public/js/player.js
@@ -1,8 +1,21 @@
-var showAndHide = function (id1, id2) {
-    $(`#${id1}`).hide
-    $(`#${id2}`).show
+let showAndHide = function (id1, id2) {
+    $(`#${id1}`).hide()
+    $(`#${id2}`).show()
 }
 
-$('#code-button').click(function() {
-    showAndHide("roomCode", "pregame")
+$('#roomCode').submit(function(e) {
+    e.preventDefault();
+    // console.log(e)
+    let inputCode = $("#code-input").val();
+    console.log(inputCode)
+    //Compare input code to room codes available
+    // if (input code exists)
+        showAndHide("roomCode", "pregame")
+    // else
+    // Request new code
 });
+
+//On game start
+    // showAndHide("pregame", "hand")
+
+//On code form submit
