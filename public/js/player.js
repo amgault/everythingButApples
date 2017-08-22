a0914838ee6e6ce97c937fa85d505792bef2dbaf
unknown <mrashes2@gmail.com>
2017-08-21 20:16:27 -0500

    sudocoded submitting code, name, and card forms

--- public/js/player.js
+++ public/js/player.js
@@ -1,11 +1,11 @@
+//To hide first div and show second
 let showAndHide = function (id1, id2) {
     $(`#${id1}`).hide()
     $(`#${id2}`).show()
 }
 
-$('#roomCode').submit(function(e) {
-    e.preventDefault();
-    // console.log(e)
+//This is to validate code
+function codeComparison() {
     let inputCode = $("#code-input").val();
     console.log(inputCode)
     //Compare input code to room codes available
@@ -13,9 +13,45 @@ $('#roomCode').submit(function(e) {
         showAndHide("roomCode", "pregame")
     // else
     // Request new code
+}
+
+function submitUsername() {
+    //I don't think there needs to be validation if name is already taken but it may mess us up.  Unless each player is named player i to our programming
+
+    //AJAX post user name to room
+        //This should post it to the host to display the name on the players box
+
+    //Shows next input if they want
+    showAndHide("username", "createCards")
+}
+
+function submitCards() {
+    //Can they make duplicates in the deck or not (?)
+
+    //AJAX post card words to room
+        //This should post it to the deck (?)
+        //I think this means a deck instance has to be made for each game.  So maybe you pull the original deck once then you post new cards into our "Sudodeck"
+    
+    //reset input so they can make more cards
+}
+
+//Code comparison listener
+$('#roomCode').submit(function(e) {
+    e.preventDefault()
+    codeComparison()
 });
+
+//Name submit listener
+$("#username").submit(function(e) {
+    e.preventDefault();
+    submitUsername()
+})
+
+//Name submit listener
+$("#createCards").submit(function(e) {
+    e.preventDefault();
+    submitCards()
+})
 
 //On game start
     // showAndHide("pregame", "hand")
-
-//On code form submit
