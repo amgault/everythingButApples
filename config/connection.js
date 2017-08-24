fe5b56a3a4a1b09127f047c6b262e5a6165c781f
smendez92 <samuelremndez14@gmail.com>
2017-08-23 20:50:01 -0500

    folder structure in place, connection.js works

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+//access environmental variables for username, password, host
+require('dotenv').config({path: '../dotenv.env'});
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "heroku_6aafb0838fd5e56"
+});
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection setup for our ORM to use.
+module.exports = connection;
