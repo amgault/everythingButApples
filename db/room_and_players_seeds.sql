5cac0744ab5aa1086deab105b4c6e3b04f38669d
smendez92 <samuelremndez14@gmail.com>
2017-08-26 12:22:28 -0500

    sql codes removed

--- db/room_and_players_seeds.sql
+++
@@ -1,5 +0,0 @@
-USE `heroku_6aafb0838fd5e56`;
-insert into rooms (roomName, gameLength) values("T3sT Ro0M", 2);
-insert into players (username, room_id) values ("playerA",1);
-insert into players (username, room_id) values ("playerB",1);
-insert into players (username, room_id) values ("playerC",1);
