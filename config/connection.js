c4ded670cedcbc5d85ead7af87d07eec97388b0e
jpeart <jpeart4@gmail.com>
2017-08-29 15:05:30 -0500

    config changed

--- config/connection.js
+++ config/connection.js
@@ -3,23 +3,23 @@ var mysql = require("mysql");
 // access environmental variables for username, password, host
 require('dotenv').config({ path: '../dotenv.env' });
 
-var connection;
+// var connection;
 
-// first try to use JAWSDB connection if it exists (for Heroku deployment)
-if (process.env.CLEARDB_DATABASE_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-}
+// // first try to use JAWSDB connection if it exists (for Heroku deployment)
+// if (process.env.CLEARDB_DATABASE_URL) {
+//     connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+// }
 
-// otherwise
-else {
-    //console.log("host: " + process.env.DB_HOST);
-    connection = mysql.createConnection({
-        host: "us-cdbr-iron-east-05.cleardb.net",
-        port: "3306",
-        user: "bc389e17ac8656",
-        password: "dc0c3ab7",
-        database: "heroku_6aafb0838fd5e56"
-    });
+// // otherwise
+// else {
+//     //console.log("host: " + process.env.DB_HOST);
+//     connection = mysql.createConnection({
+//         host: "us-cdbr-iron-east-05.cleardb.net",
+//         port: "3306",
+//         user: "bc389e17ac8656",
+//         password: "dc0c3ab7",
+//         database: "heroku_6aafb0838fd5e56"
+//     });
     var pool = mysql.createPool({
         connectionLimit: 10,
         connectTimeout: 60 * 60 * 1000,
@@ -31,6 +31,6 @@ else {
         password: "dc0c3ab7",
         database: "heroku_6aafb0838fd5e56"
     });
-}
+// }
 
 module.exports = pool;
